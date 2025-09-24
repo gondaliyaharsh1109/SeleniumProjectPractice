fdf6f517f8228e035f66e175687381e086148387
gondaliyaharsh1109 <gondaliyaharsh50@gmail.com>
2025-09-24 12:04:21 +0530

    Added new test script for department toast message with position

--- src/test/java/com/practiceautomationtest/tests/allPagesClass/DepartmentPage.java
+++ src/test/java/com/practiceautomationtest/tests/allPagesClass/DepartmentPage.java
@@ -6,17 +6,28 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 
 public class DepartmentPage extends BasePage {
     Faker faker = new Faker();
+    Actions actions = new Actions(driver);
     By newButton = By.xpath("//button[@tabindex='0']");
     By enterDepartmentName = By.id("departmentName");
     By enterLocationName = By.id("location");
     By clickSaveButton = By.xpath("//button[contains(text(),'Save')]");
     By visibleCreateDepartmentText = By.xpath("//li[@class='MuiBreadcrumbs-li']");
-    By clickOnDepartmentToEdit = By.xpath("(//div[@data-field='Department Name'])[2]");
+    By clickOnDepartmentToEdit = By.xpath("(//div[@data-field='departmentName'])[2]");
     By searchDepartment = By.xpath("//input[@placeholder='Search with DepartmentName']");
+    By verifyEditDepartmentText = By.xpath("//p[contains(text(),'Edit Department')]");
     By editDepartmentNameField = By.xpath("//input[@id='departmentName']");
+    By clickPositionTab = By.xpath("//span[contains(text(),'Position')]");
+    By verifyDepartmentNameFromPositionPage = By.xpath("(//div[@data-field='departmentName'])[2]");
+    By clickDepartmentsTab = By.xpath("//span[contains(text(),'Departments')]");
+    By loaderToBeInvisible = By.xpath("//span[contains(@role,'progressbar')]");
+    By deleteDepartmentBtn = By.xpath("(//button[contains(text(),'Delete')])[1]");
+    By verifyConfirmDeleteMessage = By.xpath("//p[contains(text(),'Do you want to delete the Department: EC Department?')]");
+    By confirmDeleteBtn = By.xpath("(//button[contains(text(),'Delete')])[3]");
+    By departmentAttachedWithPosition = By.xpath("//p[contains(text(),'This department has positions attached. Please remove positions first.')]");
 
     public DepartmentPage(WebDriver driver){
         super(driver);
@@ -47,11 +58,32 @@ public class DepartmentPage extends BasePage {
         WebElement searchDptName = waitForElement(searchDepartment);
         searchDptName.sendKeys(oldName);
         searchDptName.sendKeys(Keys.ENTER);
-        Actions actions = new Actions(driver);
         WebElement searchDepartment = waitForElement(clickOnDepartmentToEdit);
         actions.doubleClick(searchDepartment).perform();
         waitForElement(editDepartmentNameField).clear();
         waitForElement(editDepartmentNameField).sendKeys(newName);
         clickSaveButton();
+    }
+    public void verifyDeleteDepartmentValidationWithPosition(){
+        waitForElement(clickPositionTab).click();
+        String actualVerifyDepartmentName = waitForElement(verifyDepartmentNameFromPositionPage).getText();
+        String expectedDepartmentName = "EC Department";
+        Assert.assertEquals(actualVerifyDepartmentName,expectedDepartmentName);
+        waitForElement(clickDepartmentsTab).click();
+        waitForElement(searchDepartment).sendKeys(expectedDepartmentName);
+        waitForElement(searchDepartment).sendKeys(Keys.ENTER);
+        waitForElementToBeInvisible(loaderToBeInvisible);
+        actions.doubleClick(waitForElement(clickOnDepartmentToEdit)).perform();
+        String actualEditDepartmentText = waitForElement(verifyEditDepartmentText).getText();
+        String expectedEditDepartmentText = "Edit Department";
+        Assert.assertEquals(actualEditDepartmentText,expectedEditDepartmentText);
+        waitForElement(deleteDepartmentBtn).click();
+        String actualDeleteConfirmMessage = waitForElement(verifyConfirmDeleteMessage).getText();
+        String expectedDeleteConfirmMessage = "Do you want to delete the Department: EC Department?";
+        Assert.assertEquals(actualDeleteConfirmMessage,expectedDeleteConfirmMessage);
+        waitForElement(confirmDeleteBtn).click();
+        String actualDepartmentValidateMessage = waitForElement(departmentAttachedWithPosition).getText();
+        String expectedDepartmentValidateMessage = "This department has positions attached. Please remove positions first.";
+        Assert.assertEquals(actualDepartmentValidateMessage,expectedDepartmentValidateMessage);
     }
 }
