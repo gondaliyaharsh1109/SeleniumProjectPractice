0217f6adc37300adef01fbf4cfd2f74de6b3dbde
gondaliyaharsh1109 <gondaliyaharsh50@gmail.com>
2025-09-24 18:27:01 +0530

    Added new taskpageclass  and tasktest

---
+++ src/test/java/com/practiceautomationtest/tests/allPagesClass/TaskPage.java
@@ -0,0 +1,57 @@
+package com.practiceautomationtest.tests.allPagesClass;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+public class TaskPage extends BasePage{
+    BasePage basePage = new BasePage(driver);
+    Actions actions = new Actions(driver);
+    Faker faker = new Faker();
+    By clickTasksTab = By.xpath("//span[contains(text(),'Tasks')]");
+    By newButton = By.xpath("//button[contains(text(),'New')]");
+    By visibleCreateTaskText = By.xpath("//p[contains(text(),'Create Task')]");
+    By taskNameInputField = By.id("taskName");
+    By assigneeSelectBtn = By.xpath("(//button[@type='button'])[7]");
+    By assigneeEmailField = By.xpath("(//div[@data-field='firstName'])[3]");
+    By taskDateSelectionBtn = By.xpath("(//button[@type='button'])[8]");
+    By selectDate = By.xpath("//button[contains(text(),'25')]");
+    By taskDescInputField = By.id("taskDescription");
+    By saveBtn = By.xpath("//button[contains(text(),'Save')]");
+    By loaderToBeInvisible = By.xpath("//span[contains(@role,'progressbar')]");
+    By searchTaskName = By.id("search");
+    By verifyAddedTask = By.xpath("(//div[@data-field='taskName'])[2]");
+
+    public TaskPage(WebDriver driver){
+        super(driver);
+    }
+    public void addNewTask(){
+        String taskName = "Task: " + faker.company().buzzword();
+        String taskDescription = faker.lorem().sentence(10);
+
+        waitForElement(clickTasksTab).click();
+        waitForElement(newButton).click();
+        String actualVisibleCreateTaskText = waitForElement(visibleCreateTaskText).getText();
+        String expectedVisibleCreateTaskText = "Create Task";
+        Assert.assertEquals(actualVisibleCreateTaskText,expectedVisibleCreateTaskText);
+        waitForElement(taskNameInputField).sendKeys(taskName);
+        waitForElement(assigneeSelectBtn).click();
+        waitForElementToBeInvisible(loaderToBeInvisible);
+        actions.doubleClick(waitForElement(assigneeEmailField)).perform();
+        waitForElement(taskDateSelectionBtn).click();
+        waitForElement(selectDate).click();
+        waitForElement(taskDescInputField).sendKeys(taskDescription);
+        waitForElement(saveBtn).click();
+        String actualTaskCreatedMessage = basePage.verifyToastMessage();
+        String expectedTaskCreatedMessage = "Task created successfully.";
+        Assert.assertEquals(actualTaskCreatedMessage,expectedTaskCreatedMessage);
+        waitForElement(searchTaskName).sendKeys(taskName);
+        waitForElement(searchTaskName).sendKeys(Keys.ENTER);
+        waitForElementToBeInvisible(loaderToBeInvisible);
+        String actualTaskAddedName = waitForElement(verifyAddedTask).getText();
+        Assert.assertEquals(actualTaskAddedName,taskName);
+    }
+}
