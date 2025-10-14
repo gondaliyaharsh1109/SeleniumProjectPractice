a2bf704f9b23a404895c27b1d822d41af1655bd4
gondaliyaharsh1109 <gondaliyaharsh50@gmail.com>
2025-10-14 18:32:45 +0530

    edited

--- src/test/java/com/practiceautomationtest/tests/DepartmentTestPageAdmin/DepartmentTest.java
+++ src/test/java/com/practiceautomationtest/tests/DepartmentTestPageAdmin/DepartmentTest.java
@@ -3,7 +3,6 @@ import com.practiceautomationtest.tests.allPagesClassAdmin.BasePage;
 import com.practiceautomationtest.tests.allPagesClassAdmin.DepartmentPage;
 import com.practiceautomationtest.tests.allPagesClassAdmin.LoginPage;
 import com.practiceautomationtest.tests.BaseTest;
-import io.qameta.allure.Allure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,6 +10,7 @@ public class DepartmentTest extends BaseTest {
 
     @Test
     public void addDepartment(){
+        test = extent.createTest("Add department");
         BasePage basePage = new BasePage(driver);
         LoginPage loginPage = new LoginPage(driver);
         DepartmentPage departmentPage = new DepartmentPage(driver);
@@ -21,6 +21,7 @@ public class DepartmentTest extends BaseTest {
     }
     @Test
     public void notAbleToDeleteDepartmentWithPosition(){
+        test = extent.createTest("Not able to delete department with position");
         BasePage basePage = new BasePage(driver);
         LoginPage loginPage = new LoginPage(driver);
         DepartmentPage departmentPage = new DepartmentPage(driver);
@@ -31,6 +32,7 @@ public class DepartmentTest extends BaseTest {
     }
     @Test
     public void searchingDepartmentUsingDepartmentNameAndLocationFilter(){
+        test = extent.createTest("Search department using name and location filter");
         BasePage basePage = new BasePage(driver);
         LoginPage loginPage = new LoginPage(driver);
         DepartmentPage departmentPage = new DepartmentPage(driver);
@@ -41,6 +43,7 @@ public class DepartmentTest extends BaseTest {
     }
     @Test
     public void searchingDepartmentUsingLocationFilterOnly(){
+        test = extent.createTest("Search department using location filer");
         BasePage basePage = new BasePage(driver);
         LoginPage loginPage = new LoginPage(driver);
         DepartmentPage departmentPage = new DepartmentPage(driver);
@@ -51,6 +54,7 @@ public class DepartmentTest extends BaseTest {
     }
     @Test
     public void updateStatusOfDepartmentFromActiveToInactiveAndVerifyBySearch(){
+        test = extent.createTest("Update status of department from active to inactive and verify by search");
         BasePage basePage = new BasePage(driver);
         LoginPage loginPage = new LoginPage(driver);
         DepartmentPage departmentPage = new DepartmentPage(driver);
@@ -61,6 +65,7 @@ public class DepartmentTest extends BaseTest {
     }
     @Test
     public void redirectToDepartmentByClickingHomeBtn(){
+        test = extent.createTest("Redirect to department by clicking home btn");
         DepartmentPage departmentPage = new DepartmentPage(driver);
         departmentPage.redirectToDepartmentByClickingHomeBtn();
     }
